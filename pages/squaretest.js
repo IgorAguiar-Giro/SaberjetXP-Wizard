ebecd3003010bc27de15784a53844f6faf829005
Igor Aguiar <igoraguiar00@gmail.com>
2023-07-04 20:05:53 -0400

    square test under construction

--- pages/squaretest.js
+++ pages/squaretest.js
@@ -1,5 +1,32 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "../assets/NavBar";
 
 export default function SquareTest() {
-  return <h1>Square Test</h1>;
+  return (
+    <>
+      <NavBar />
+      <body className="">
+        <div className="container p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3 ">
+          <h1 className="--bs-danger-text-emphasis">
+            The Square Test Calculator
+          </h1>
+          <p>
+            If you don't know how to setup your machine for the square test,
+            follow this link{" "}
+            <a
+              target="_blank"
+              href="https://www.parkindustries.com/stone/service/blade-offset-cut/"
+            >
+              https://www.parkindustries.com/stone/service/blade-offset-cut/
+            </a>
+          </p>
+          <p>
+            If you don't know english and can't understand the video above I
+            will explaing shortly how to do it step by step in this page
+          </p>
+        </div>
+      </body>
+    </>
+  );
 }
